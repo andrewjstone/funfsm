8045487077ba8bbbca9a16bc013305e6d2f05530
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2015-12-08 19:45:39 -0500

    Remove ThreadedFsm, LocalFsm and Fsm trait
    Now there is only a single FSM type. This prevents the necessity of
    using trait objects when trying to be transparent about the type. The
    new type is just called Fsm and it behaves the same as the old LocalFsm.
    
    Also remove the tracing code.

--- src/fsm.rs
+++ src/fsm.rs
@@ -7,19 +7,6 @@ macro_rules! next {
      }
 }
 
-#[derive(Clone, Debug)]
-pub enum FsmType {
-    Local,
-    Threaded
-}
-
-pub trait Fsm<T: FsmHandler> {
-    fn get_state(&self) -> (&'static str, T::Context);
-    fn send_msg(&mut self, msg: T::Msg);
-    fn trace_on(&mut self, _path: &str);
-    fn trace_off(&mut self);
-}
-
 // A recursive tuple struct indicating the name of current state and the function pointer that
 // handles messages in that that state. Calling that function returns the next state in a
 // StateFn<T>.
@@ -27,6 +14,38 @@ pub struct StateFn<T: FsmHandler>(
     pub &'static str,
     pub fn(&mut T::Context, T::Msg) -> StateFn<T>
 );
+
+// Function pointers aren't `Clone` due to this [bug](https://github.com/rust-lang/rust/issues/24000)
+// Since we can't derive clone, we just implement it manually, since function pointers are `Copy`
+impl<T: FsmHandler> Clone for StateFn<T> {
+    fn clone(&self) -> StateFn<T> {
+        StateFn(self.0, self.1)
+    }
+}
+
+#[derive(Clone)]
+pub struct Fsm<T: FsmHandler> {
+    pub state: StateFn<T>,
+    pub ctx: T::Context
+}
+
+impl<T: FsmHandler> Fsm<T> {
+    pub fn new(ctx: T::Context) -> Fsm<T> {
+        Fsm {
+            state: T::initial_state(),
+            ctx: ctx,
+        }
+    }
+
+    pub fn get_state(&self) -> (&'static str, T::Context) {
+        (self.state.0, self.ctx.clone())
+    }
+
+    pub fn send_msg(&mut self, msg: T::Msg) {
+        let StateFn(_name, f) = self.state;
+        self.state = f(&mut self.ctx, msg);
+    }
+}
 
 pub trait FsmHandler: Sized {
     // The application state of the fsm
