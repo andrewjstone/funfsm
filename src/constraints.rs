287624bd6e11a55b6522552214463c983159d44f
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2017-01-11 04:40:30 -0500

    Remove postcondition constraint and enhance transition
    The transition constraint function now is able to perform much more
    invariant checking since it has more information to work with. This
    function makes a postcondition constraint unnecessary.
    
    Add usage docs to README
    
    Bump version to 0.2

--- src/constraints.rs
+++ src/constraints.rs
@@ -1,67 +1,88 @@
 use std::collections::HashMap;
+use fsm::FsmTypes;
 
-pub type Pred<T> = Box<Fn(&T) -> bool>;
+pub type Pred<T: FsmTypes> = Box<Fn(&T::Context) -> bool>;
+pub type TransitionCheck<T: FsmTypes>
+    = fn(&T::Context, &T::Context, &T::Msg, &Vec<T::Output>) -> Result<(), String>;
 
-pub struct Constraints<T> {
+pub struct Constraints<T: FsmTypes> {
     pub preconditions: HashMap<&'static str, Vec<(Pred<T>, String)>>,
-    pub postconditions: HashMap<&'static str, Vec<(Pred<T>, String)>>,
     pub invariants: Vec<(Pred<T>, String)>,
-    pub transitions: HashMap<(&'static str, &'static str), Vec<(Pred<T>, String)>>
+    pub transitions: HashMap<(&'static str, &'static str), TransitionCheck<T>>
 }
 
-impl<T> Constraints<T> {
+impl<T: FsmTypes> Constraints<T> {
     pub fn new() -> Constraints<T> {
         Constraints {
             preconditions: HashMap::new(),
-            postconditions: HashMap::new(),
             invariants: Vec::new(),
             transitions: HashMap::new()
         }
     }
 
-    pub fn check_preconditions(&self, state: &'static str, ctx: &T) -> Result<(), String> {
-        Constraints::check_map(&self.preconditions, state, ctx)
+    pub fn check_preconditions(&self, state: &'static str, ctx: &T::Context) -> Result<(), String> {
+        Constraints::<T>::check_map(&self.preconditions, state, ctx)
     }
 
-    pub fn check_postconditions(&self, state: &'static str, ctx: &T) -> Result<(), String> {
-        Constraints::check_map(&self.postconditions, state, ctx)
+    pub fn check_invariants(&self, ctx: &T::Context) -> Result<(), String> {
+        Constraints::<T>::check_vec(&self.invariants, ctx)
     }
 
-    pub fn check_invariants(&self, ctx: &T) -> Result<(), String> {
-        Constraints::check_vec(&self.invariants, ctx)
-    }
-
-    pub fn check_transitions(&self, from: &'static str, to: &'static str, ctx: &T)
-        -> Result<(), String> {
-
+    /// Verify a transition result
+    ///
+    ///  `from` is the from state,
+    ///  `to` is the to state,
+    ///  `init_ctx` is the internal data state of the fsm **before** the transition
+    ///  `final_ctx` is the internal data state of the fsm **after** the transition
+    ///  `msg` is the message that caused the transition
+    ///  `output` is the output messages as a result of the transition
+    ///
+    ///  Returns an error string if the transition check fails
+    pub fn check_transition(&self,
+                            from: &'static str,
+                            to: &'static str,
+                            init_ctx: &T::Context,
+                            final_ctx: &T::Context,
+                            msg: &T::Msg,
+                            output: &Vec<T::Output>) -> Result<(), String>
+    {
         match self.transitions.get(&(from, to)) {
-            // TODO: This one is interesting.
-            // If there isn't a specific transition listed should we fail it?
-            // Keep in mind that if there are no restraints on transition, the predicate could
-            // just always return true
             None => Ok(()),
-            Some(functions) => {
-                Constraints::check_vec(functions, ctx)
+            Some(check) => {
+                check(init_ctx, final_ctx, msg, output)
             }
         }
     }
 
     fn check_map(map: &HashMap<&'static str, Vec<(Pred<T>, String)>>,
                  state: &'static str,
-                 ctx: &T) -> Result<(), String> {
+                 ctx: &T::Context) -> Result<(), String> {
         match map.get(state) {
             None => Ok(()),
             Some(functions) => {
-                Constraints::check_vec(functions, ctx)
+                Constraints::<T>::check_vec(functions, ctx)
             }
         }
     }
 
-    fn check_vec(vec: &Vec<(Pred<T>, String)>, ctx: &T) -> Result<(), String> {
+    fn check_vec(vec: &Vec<(Pred<T>, String)>, ctx: &T::Context) -> Result<(), String> {
         for &(ref f, ref msg) in vec {
             if !f(ctx) { return Err((*msg).clone()); }
         }
         Ok(())
+    }
+}
+
+#[macro_export]
+macro_rules! check {
+    ($err:expr, $predicate:expr) => {
+        if $predicate {
+            let res: Result<(), String> = Ok(());
+            res
+        } else {
+            return Err(format!("Error: {} Predicate: {} File: {} Line: {}",
+                               $err, stringify!($predicate), file!(), line!()));
+        }
     }
 }
 
@@ -78,17 +99,6 @@ macro_rules! precondition {
     }}
 }
 
-/// Nearly identical to precondition!
-#[macro_export]
-macro_rules! postcondition {
-    ($c:ident, $s:expr, $p:expr) => {{
-        let f = Box::new($p);
-        let err = constraints::errstr("postcondition", $s, stringify!($p));
-        let mut vec = $c.postconditions.entry($s).or_insert(Vec::new());
-        vec.push((f, err));
-    }}
-}
-
 /// Pre/Postconditions are only checked in specific states. Invariants are checked in every state.
 #[macro_export]
 macro_rules! invariant {
@@ -101,12 +111,8 @@ macro_rules! invariant {
 
 #[macro_export]
 macro_rules! transition {
-    ($c:ident, $f:expr, $t:expr, $p:expr) => {{
-        let f = Box::new($p);
-        let err = format!("Failed transition predicate from state {} to {}: {}",
-                          $f, $t, stringify!($p));
-        let mut vec = $c.transitions.entry(($f, $t)).or_insert(Vec::new());
-        vec.push((f, err));
+    ($constraints:ident, $from:expr => $to:expr, $check:expr) => {{
+        $constraints.transitions.insert(($from, $to), $check);
     }}
 }
 
--- src/fsm_check.rs
+++ src/fsm_check.rs
@@ -3,37 +3,37 @@ use constraints::Constraints;
 
 pub struct Checker<T: FsmTypes> {
     fsm: Fsm<T>,
-    constraints: Constraints<T::Context>
+    constraints: Constraints<T>
 }
 
 impl<T: FsmTypes> Checker<T> {
-    pub fn new(ctx: T::Context, state: StateFn<T>, constraints: Constraints<T::Context>) -> Checker<T> {
+    pub fn new(ctx: T::Context, state: StateFn<T>, constraints: Constraints<T>) -> Checker<T> {
         Checker {
             fsm: Fsm::<T>::new(ctx, state),
             constraints: constraints
         }
     }
 
-    // TODO: Use quickcheck and a generator for messages here
-    pub fn check(&mut self, msgs: Vec<T::Msg>) -> Result<(), String> {
-        for msg in msgs {
-            let from = try!(self.check_preconditions());
-            self.fsm.send(msg);
-            try!(self.check_postconditions(from));
-        }
-        Ok(())
+    pub fn check(&mut self, msg: T::Msg) -> Result<Vec<T::Output>, String> {
+        let (from, init_ctx) = try!(self.check_preconditions());
+        let output = self.fsm.send(msg.clone());
+        self.check_postconditions(from, &init_ctx, &msg, &output).map(|_| output)
     }
 
-    pub fn check_preconditions(&self) -> Result<&'static str, String> {
+    pub fn check_preconditions(&self) -> Result<(&'static str, T::Context), String> {
         let (from, ctx) = self.fsm.get_state();
         try!(self.constraints.check_preconditions(from, &ctx));
-        Ok(from)
+        try!(self.constraints.check_invariants(&ctx));
+        Ok((from, ctx.clone()))
     }
 
-    pub fn check_postconditions(&self, from: &'static str) -> Result<(), String> {
-        let (to, ctx) = self.fsm.get_state();
-        try!(self.constraints.check_postconditions(from, &ctx));
-        try!(self.constraints.check_invariants(&ctx));
-        self.constraints.check_transitions(from, to, &ctx)
+    pub fn check_postconditions(&self,
+                                from: &'static str,
+                                init_ctx: &T::Context,
+                                msg: &T::Msg,
+                                output: &Vec<T::Output>) -> Result<(), String> {
+        let (to, final_ctx) = self.fsm.get_state();
+        try!(self.constraints.check_invariants(&final_ctx));
+        self.constraints.check_transition(from, to, init_ctx, final_ctx, msg, output)
     }
 }
